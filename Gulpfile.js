4076ac38c92138514cb4d8993261463d2a825f23
Lukasz Plotnicki <lplotnicki@thoughtworks.com>
2015-08-04 09:28:27 +0200

    Added gulp for running tests, jshint and starting pace.

---
+++ Gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var jasmine = require('gulp-jasmine');
+var jshint = require('gulp-jshint');
+
+gulp.task('lint', function() {
+  return gulp.src(['app.js','./routes/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default', { verbose: true }));
+});
+
+gulp.task('express', function() {
+  var debug = require('debug')('generated');
+  var app = require('./app.js');
+
+  app.set('port', process.env.PORT || 3000);
+
+  var server = app.listen(app.get('port'), function() {
+    debug('Express server listening on port ' + server.address().port);
+  });
+});
+
+gulp.task('test', function(){
+  return gulp.src('spec/*.js').pipe(jasmine());
+});
+
+gulp.task('default', ['express'], function() {
+});
--- README.md
+++ README.md
@@ -6,4 +6,10 @@ Pace is currently based on [express.js](http://expressjs.com/) and [node](https:
 your machine. To install node, go either to https://nodejs.org/ or if you are using a mac & brew use the following cmd: `brew install node`. To get all the other dependencies and be able to start **pace** locally, just navigate to the pace folder and run `npm install`
 
 ### Starting
-Just run `npm start` and open http://localhost:3000 in your browser. 
+Just run `gulp` and open http://localhost:3000 in your browser.
+
+### Running tests
+Just this: `gulp test`
+
+### Running jshint
+Here you go: `gulp lint`

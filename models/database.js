97f655f6ffb739a009499e61b878baa88d22168c
Diego <dpeletei@thoughtworks.com>
2015-08-09 16:08:08 +0200

    Diego - removed database constraint for mandatory fields

--- models/database.js
+++ models/database.js
@@ -4,7 +4,7 @@ client.connect(
     function (err) { 
         if (!err) {
             return client.query (
-                "create table participants (id SERIAL PRIMARY KEY, email VARCHAR(255) not null, first_name VARCHAR(255) not null, last_name VARCHAR(255) not null);", 
+                "create table participants (id SERIAL PRIMARY KEY, email VARCHAR(255), first_name VARCHAR(255), last_name VARCHAR(255));",
                 function (err, res) { 
                     if (! err) { 
                         console.log("result:" , res);

04b8925f4faa2116564c1822e0c0a8955d978f43
Lukasz Plotnicki <lplotnicki@thoughtworks.com>
2015-09-21 17:08:57 +0200

    moved the function init. to the before block.

--- spec/registrationSpec.js
+++ spec/registrationSpec.js
@@ -21,8 +21,6 @@ describe('registration on post', function () {
     });
 
     it('should throw an error if no firstname can be found', function () {
-        var extractParticipant = require('../routes/registration.js').extractParticipant;
-
         function callWithNoFirstname() {
             extractParticipant({body: {}});
         }
@@ -31,8 +29,6 @@ describe('registration on post', function () {
     });
 
     it('should throw an error if no lastname can be found', function () {
-        var extractParticipant = require('../routes/registration.js').extractParticipant;
-
         function callWithNoFirstname() {
             extractParticipant({body: {firstName: 'XX'}});
         }
@@ -41,8 +37,6 @@ describe('registration on post', function () {
     });
     
     it('should throw an error if no email can be found', function () {
-        var extractParticipant = require('../routes/registration.js').extractParticipant;
-
         function callWithNoEmail() {
             extractParticipant({body: {firstName: 'XX', lastName: 'YY'}});
         }
@@ -58,6 +52,4 @@ describe('registration on post', function () {
     it('should read email from the request body', function () {
         expect(extractParticipant(validRequestData).email).toBe('m.mueller@example.com');
     });
-
-
 });

4991308eb863994b9e49bb6e7ef97434c2c97309
Lukasz Plotnicki <lplotnicki@thoughtworks.com>
2015-10-04 19:23:17 +0200

    Simplified & fixed by the done() call.

--- models/database.js
+++ models/database.js
@@ -1,20 +1,10 @@
 var pg = require('pg');
 var connectionString = process.env.SNAP_DB_PG_URL || process.env.DATABASE_URL || 'tcp://vagrant@localhost/pace';
-var client = new pg.Client(connectionString);
-client.connect(
-    function (err) {
-        console.log(err);
+pg.connect(connectionString, function (err, client, done) {
         if (!err) {
-            return client.query (
-                "create table participants (id SERIAL PRIMARY KEY, email VARCHAR(255), firstname VARCHAR(255), lastname VARCHAR(255));",
-                function (err, res) { 
-                    if (! err) { 
-                        console.log("result:" , res);
-                        client.end();
-                        return "created";
-                    }
-                }
-            );
+            client.query (
+                "create table participants (id SERIAL PRIMARY KEY, email VARCHAR(255), firstname VARCHAR(255), lastname VARCHAR(255));");
+            done();
         }
     }
 );

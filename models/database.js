e304849d359f719e47018cd26954af886257a3db
Lukasz Plotnicki <lplotnicki@thoughtworks.com>
2015-10-05 07:14:10 +0200

    Added better error handling.

--- models/database.js
+++ models/database.js
@@ -2,9 +2,19 @@ var pg = require('pg');
 var connectionString = process.env.SNAP_DB_PG_URL || process.env.DATABASE_URL || 'tcp://vagrant@localhost/pace';
 pg.connect(connectionString, function (err, client, done) {
         if (!err) {
-            client.query (
+            var query = client.query (
                 "create table participants (id SERIAL PRIMARY KEY, email VARCHAR(255), firstname VARCHAR(255), lastname VARCHAR(255));");
-            done();
+            query.on('error', function(error) {
+                console.error('Could not execute the query: ', error);
+                client.end();
+            });
+            query.on('end', function() {
+                console.log('CREATE DB done');
+                client.end();
+            })
+        } else {
+            console.error('Problem with the DB connection: ', err);
+            client.end();
         }
     }
 );

cb69305088173ea94f62a50f159513fe67064f1e
Lukasz Plotnicki <lplotnicki@thoughtworks.com>
2015-10-05 17:28:37 +0200

    Shutting down node gracefully

--- models/database.js
+++ models/database.js
@@ -1,20 +1,26 @@
 var pg = require('pg');
 var connectionString = process.env.SNAP_DB_PG_URL || process.env.DATABASE_URL || 'tcp://vagrant@localhost/pace';
+
+function exit(client) {
+    client.end();
+    process.exit();
+}
+
 pg.connect(connectionString, function (err, client, done) {
         if (!err) {
             var query = client.query (
                 "create table participants (id SERIAL PRIMARY KEY, email VARCHAR(255), firstname VARCHAR(255), lastname VARCHAR(255));");
             query.on('error', function(error) {
                 console.error('Could not execute the query: ', error);
-                client.end();
+                exit(client);
             });
             query.on('end', function() {
                 console.log('CREATE DB done');
-                client.end();
+                exit(client);
             })
         } else {
             console.error('Problem with the DB connection: ', err);
-            client.end();
+            exit(client);
         }
     }
 );

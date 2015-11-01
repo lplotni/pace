82f1792a5adc15293f6cc2af68f4dbf81eab78d0
mduemcke <mduemcke@thoughtworks.com>
2015-11-01 17:25:21 +0100

    restricted access to admin page to logged in users
    ...redirects to login page if the user is not logged in

--- spec/adminJourney.js
+++ spec/adminJourney.js
@@ -27,9 +27,25 @@ describe('admin page', function() {
 
     it('should go to admin page and show admin links', function (done) {
         client.init()
+            .url(paceUrl + 'login')
+            .setValue('input#username', 'admin')
+            .setValue('input#password', 'admin')
+            .click('button#submit')
             .url(paceUrl)
             .click('a#adminPage')
             .isVisible('a#paymentValidation')
+            .then(function(isVisible) {
+                expect(isVisible).toBe(true);
+                done();
+            })
+            .end();
+    });
+
+    it('should redirect to login page if the user is not logged in', function (done) {
+        client.init()
+            .url(paceUrl)
+            .click('a#adminPage')
+            .isVisible('form#loginForm')
             .then(function(isVisible) {
                 expect(isVisible).toBe(true);
                 done();

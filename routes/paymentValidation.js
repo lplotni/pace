bcd8c7357e52f5e566e124113d0b50e9f3dc3655
Lukasz Plotnicki <l.plotnicki@gmail.com>
2015-12-13 14:17:40 +0100

    [indentation] Using 2 spaces instead of 4.

--- routes/paymentValidation.js
+++ routes/paymentValidation.js
@@ -6,70 +6,70 @@ var participants = require('../service/participants');
 var accesscontrol = require('../acl/accesscontrol');
 var isAuthenticated = require('../acl/authentication');
 
-var canConfirmPayments = function(role) {
-    return accesscontrol.hasPermissionTo(role, 'confirm payments');
+var canConfirmPayments = function (role) {
+  return accesscontrol.hasPermissionTo(role, 'confirm payments');
 };
 
 router.get('/', isAuthenticated, function (req, res) {
-    if(canConfirmPayments(req.user.role)) {
-        participants.getRegistered().then(function(result){
-          res.render('paymentValidation/paymentValidation', {participants: result});
+  if (canConfirmPayments(req.user.role)) {
+    participants.getRegistered().then(function (result) {
+      res.render('paymentValidation/paymentValidation', {participants: result});
+    });
+  } else {
+    var result = {
+      message: 'Bitte anmelden',
+      error: {
+        status: 'Nur Administratoren können diese Seite einsehen'
+      }
+    };
+    res.render('error', result);
+  }
+});
+
+router.post('/', isAuthenticated, function (req, res) {
+  if (canConfirmPayments(req.user.role)) {
+    var paymentToken = req.body.paymenttoken;
+
+    participants.getByToken(paymentToken)
+      .then(function (result) {
+        return res.render('paymentValidation/paymentValidation', {
+          token: paymentToken,
+          name: result.name,
+          amount: result.amount,
+          participantid: result.id
         });
-    } else {
-        var result = {
-            message: 'Bitte anmelden',
-            error: {
-                status: 'Nur Administratoren können diese Seite einsehen'
-            }
-        };
-        res.render('error', result);
-    }
+      })
+      .catch(function (result) {
+        return res.render('paymentValidation/paymentValidation', {
+          token: paymentToken,
+          error: result.error
+        });
+      });
+  }
 });
 
-router.post('/', isAuthenticated, function(req, res) {
-    if(canConfirmPayments(req.user.role)) {
-        var paymentToken = req.body.paymenttoken;
-
-        participants.getByToken(paymentToken)
-            .then(function (result) {
-                return res.render('paymentValidation/paymentValidation', {
-                    token: paymentToken,
-                    name: result.name,
-                    amount: result.amount,
-                    participantid: result.id
-                });
-            })
-            .catch(function (result) {
-                return res.render('paymentValidation/paymentValidation', {
-                    token: paymentToken,
-                    error: result.error
-                });
-            });
-    }
-});
-
-router.post('/confirm', isAuthenticated, function(req, res) {
-    if(canConfirmPayments(req.user.role)) {
-        participants.confirmParticipant(req.body.participantid)
-            .then(function () {
-                res.render('paymentValidation/paymentValidation', {
-                    successMessage: 'Der Teilnehmer wurde bestätigt',
-                    token: req.body.paymenttoken,
-                    name: req.body.name,
-                    amount: req.body.amount,
-                    participantid: req.body.participantid
-                });
-            })
-            .catch(function () {
-                res.render('paymentValidation/paymentValidation', {
-                    error: 'Fehler: Der Teilnehmer konnte nicht bestätigt werden',
-                    token: req.body.paymenttoken,
-                    name: req.body.name,
-                    amount: req.body.amount,
-                    participantid: req.body.participantid
-                });
-            });
-    }
+router.post('/confirm', isAuthenticated, function (req, res) {
+  if (canConfirmPayments(req.user.role)) {
+    participants.confirmParticipant(req.body.participantid)
+      .then(function () {
+        res.render('paymentValidation/paymentValidation', {
+          successMessage: 'Der Teilnehmer wurde bestätigt',
+          token: req.body.paymenttoken,
+          name: req.body.name,
+          amount: req.body.amount,
+          participantid: req.body.participantid
+        });
+      })
+      .catch(function () {
+        res.render('paymentValidation/paymentValidation', {
+          error: 'Fehler: Der Teilnehmer konnte nicht bestätigt werden',
+          token: req.body.paymenttoken,
+          name: req.body.name,
+          amount: req.body.amount,
+          participantid: req.body.participantid
+        });
+      });
+  }
 });
 
 module.exports = router;

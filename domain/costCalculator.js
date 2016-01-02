ac8229b25b138cf1fa6c3abef4b69cc6a09c3d25
mduemcke <mduemcke@thoughtworks.com>
2016-01-02 17:25:29 +0100

    displayed amount to pay in the participant's list

--- domain/costCalculator.js
+++ domain/costCalculator.js
@@ -8,7 +8,7 @@ const config = require('config');
 let calculator = {};
 
 calculator.priceFor = function (participant) {
-  if (_.isEmpty(participant.tshirt)) {
+  if (_.isEmpty(participant.tshirt) || participant.tshirt.amount === 0) {
     return parseFloat(config.get('costs.standard'));
   } else {
     return parseFloat(config.get('costs.standard')) + parseFloat(config.get('shirts.price'));

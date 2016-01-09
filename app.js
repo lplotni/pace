eb649d8d3bb9bfb7dcad7ac2662143ff9d5432e4
Christoph <ckluente@thoughtworks.com>
2016-01-09 23:19:56 +0100

    don't tell the attacker we are running node
    security by obscurity, but easy to implement and
    doesn't hurt at all

--- app.js
+++ app.js
@@ -29,6 +29,8 @@ app.locals.node_env = process.env.NODE_ENV;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.disable("x-powered-by");
+
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());

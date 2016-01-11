d70042d4da8b0d4ef3a2c026d435c73b129000df
Christoph <ckluente@thoughtworks.com>
2016-01-11 22:00:11 +0100

    redirect instead of showing success

--- routes/participants/editParticipant.js
+++ routes/participants/editParticipant.js
@@ -38,7 +38,7 @@ router.post('/delete', (req, res) => {
   if (canDeleteUser(req.user.role)) {
     const id = req.body.participantid;
     participants.delete(id)
-      .then(() => res.render('participants/delete', {}))
+      .then(() => res.redirect('/participants'))
       .catch(() => res.render('error', {message: "Es ist ein Fehler aufgetreten", error: {status: "Bitte versuche es nochmal"}}));
   } else {
     res.render('error', {

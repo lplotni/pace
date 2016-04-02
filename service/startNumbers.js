e7308d3609015c7343821e99a6d27cb9020c2f48
Lukasz Plotnicki <l.plotnicki@gmail.com>
2016-04-02 12:41:55 +0200

    Basic startNumber generation service

---
+++ service/startNumbers.js
@@ -0,0 +1,27 @@
+/* jshint node: true */
+/* jshint esnext: true */
+'use strict';
+const db = require('../service/dbHelper');
+const _ = require('lodash');
+
+let service = {};
+
+service.next = function () {
+  return db.select('SELECT MAX(start_number) FROM participants', []).then((max) => {
+    let number = parseInt(max);
+    if (number) {
+      return service.escape(number + 1);
+    } else {
+      return 1;
+    }
+  });
+};
+
+service.escape = function (nr) {
+  if (_.includes([18, 28, 74, 84, 88, 444, 191, 192, 198, 420, 1919, 1488, 1681], nr)) {
+    return service.escape(nr + 1);
+  }
+  return nr;
+};
+
+module.exports = service;

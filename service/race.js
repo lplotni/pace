14510671b4894e37c7a0e69d2458524b431dc600
Christoph <chris@inferno.nadir.org>
2016-04-08 21:54:22 +0200

    rename registration table to a more generic name

--- service/race.js
+++ service/race.js
@@ -7,14 +7,14 @@ const _ = require('lodash');
 let race = {};
 
 race.startTime = function () {
-  return db.select("SELECT data->>'starttime' as starttime FROM registration;")
+  return db.select("SELECT data->>'starttime' as starttime FROM race;")
     .then( result => {
       return result[0].starttime;
     });
 };
 
 race.setStartTime = function (date) {
-  let query = "UPDATE registration SET data = jsonb_set(data, '{starttime}','" + date + "');"
+  let query = "UPDATE race SET data = jsonb_set(data, '{starttime}','" + date + "');"
   return db.update(query);
 };
 

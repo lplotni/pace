846fd8ba0917c60098b01c53bb337dc0bcf27a50
mduemcke <mduemcke@thoughtworks.com>
2016-05-01 15:56:17 +0200

    exclude on-site registrations from "confirmed" and "unconfirmed" query

--- service/participants.js
+++ service/participants.js
@@ -16,9 +16,9 @@ let participants = {};
 
 participants.allWithPaymentStatus = function (paymentStatus) {
   if (_.isUndefined(paymentStatus)) {
-    return db.select('select * from participants where is_on_site_registration = true order by firstname,lastname');
+    return db.select('select * from participants where is_on_site_registration = false order by firstname,lastname');
   } else {
-    return db.select('select * from participants where has_payed = $1 order by firstname,lastname', [paymentStatus]);
+    return db.select('select * from participants where has_payed = $1 and is_on_site_registration = false order by firstname,lastname', [paymentStatus]);
   }
 };
 

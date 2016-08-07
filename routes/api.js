e314bdbe1b4c433e02671690b8faa42dcb69b2fd
Christoph <chris@inferno.nadir.org>
2016-08-07 21:42:25 +0200

    makes token validation nicer

--- routes/api.js
+++ routes/api.js
@@ -3,16 +3,20 @@
 'use strict';
 
 const router = require('express').Router();
-const _ = require('lodash');
 const config = require('config');
 const participants = require('../service/participants');
 
-function checkToken(req) {
-  return req.get('X-Pace-Token') === config.get('admin.token');
-}
+let tokenValidator = function(req,res,next) {
+  if (req.get('X-Pace-Token') === config.get('admin.token')) {
+    return next();
+  } else {
+     res.setHeader('Content-Type', 'application/json');
+     res.status(403) 
+       .send(JSON.stringify({ status: 'Not allowed' }));
+  }
+};
 
-router.post('/scan', (req, res) => {
-  if ( checkToken(req)) {
+router.post('/scan',tokenValidator, (req, res) => {
     participants.updateTime (req.body.startnumber,req.body.time)
     .then((result)  => {
       res.setHeader('Content-Type', 'application/json');
@@ -23,11 +27,6 @@ router.post('/scan', (req, res) => {
      res.status(404) 
        .send(JSON.stringify({ status: 'Not Found' }));
    });
-  } else {
-     res.setHeader('Content-Type', 'application/json');
-     res.status(403) 
-       .send(JSON.stringify({ status: 'Not allowed' }));
-  }
 });
 
 module.exports = router;

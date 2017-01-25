ac42714ff5a2f458b392a903b036a1f08d5b182b
Christoph <chris@inferno.nadir.org>
2017-01-25 16:04:01 +0100

    mark participants as saved

--- routes/admin/statement-import.js
+++ routes/admin/statement-import.js
@@ -6,6 +6,7 @@ const multiparty = require('multiparty');
 const router = require('express').Router();
 const isAuthenticated = require('../../acl/authentication');
 const race = require('../../service/race');
+const participants = require('../../service/participants');
 
 router.post('/', isAuthenticated, (req, res) => {
   const form = new multiparty.Form();
@@ -13,9 +14,11 @@ router.post('/', isAuthenticated, (req, res) => {
   form.on('file', function (name, file) {
     race.parsePaymentCSV(file.path)
       .then((tokens) => {
-        res.render('admin/statement-import', {tokens: tokens});
-      }
-    )
+          tokens.forEach((token,index,tokens) => { 
+            participants.markPayedByToken(token);
+          });
+          res.render('admin/statement-import', {tokens: tokens});
+      });
   });
   form.on('error', function (err) {
     console.log(err);

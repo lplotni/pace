be17b9877405c603b4d5cba1a9dd07db71d88e38
Lukasz Plotnicki <l.plotnicki@gmail.com>
2017-03-31 15:48:52 +0200

    msg -> pdf

--- pace-pdf/pdfRequests.js
+++ pace-pdf/pdfRequests.js
@@ -1,21 +1,20 @@
 /* jshint node: true */
 /* jshint esnext: true */
 'use strict';
-
+let pdf = require('./pdf/pdfGeneration');
 let pdfRequests = {};
 
 pdfRequests.setup = (redis) => {
-  redis.subscribe('pace-pdf', (err, count) => {
-  });
-  redis.on('message', pdfRequests.parse);
+  redis.subscribe('pace-pdf', (err, count) => {});
+  redis.on('message', pdfRequests.process);
+};
+
+pdfRequests.process = (msg) => {
+  pdf.generate(pdfRequests.parse(msg));
 };
 
 pdfRequests.parse = (message) => {
  return JSON.parse(message);
 };
 
-//redis.subscribe('pdf', 'info', (err, count) => {});
-
-
 module.exports = pdfRequests;
-

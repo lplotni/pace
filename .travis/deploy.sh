f684a57de6cbbc62c85f88a72af3c8d9b6c96149
Lukasz Plotnicki <l.plotnicki@gmail.com>
2017-03-31 11:11:21 +0200

    Taging the docker images
    and pushing to the correct repo

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -7,5 +7,10 @@ set -u # Disallow unset variables
 if [ $TRAVIS_BRANCH = "master" ]
 then
   docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-  docker push lplotni/pace
+  docker tag pace_pace-app lplotni/pace-app:latest
+  docker tag pace_pace-app lplotni/pace-app:$TRAVIS_COMMIT
+  docker push lplotni/pace-app
+  docker tag pace_pace-pdf lplotni/pace-pdf:latest
+  docker tag pace_pace-pdf lplotni/pace-pdf:$TRAVIS_COMMIT
+  docker push lplotni/pace-pdf
 fi

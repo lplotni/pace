8a3666aab4d115f2907a10ce17e505c93f15ace8
bstein <bstein@thoughtworks.com>
2018-01-13 16:25:36 +0100

    throw error when birthyear is negative or in a non-integer format

--- domain/participant.js
+++ domain/participant.js
@@ -5,23 +5,31 @@
 const _ = require('lodash');
 const validator = require('validator');
 const tshirt = require('./tshirt');
-const tshirts = require('../service/tshirts');
-const participants = require('../service/participants');
 
 const participant = {};
 
 
 participant.invalidData = (body) => {
-  if (_.isEmpty(body.email) || _.isUndefined(body.email)) {
-    return _.isUndefined(body.category) || _.isUndefined(body.visibility) ;
+  const emailMissing = _.isEmpty(body.email) || _.isUndefined(body.email);
+  if (emailMissing) {
+    return _.isUndefined(body.category) || _.isUndefined(body.visibility);
   } else {
-    return !validator.isEmail(body.email) || _.isUndefined(body.category) || _.isUndefined(body.visibility) ;
+    return !validator.isEmail(body.email) || _.isUndefined(body.category) || _.isUndefined(body.visibility);
   }
+};
+
+participant.invalidYear = (birthyear) => {
+  const isValidNumber = _.isFinite(_.toNumber(birthyear));
+  return !_.isUndefined(birthyear) && (!isValidNumber || _.toNumber(birthyear) <= 0);
 };
 
 participant.from = (body) => {
   if (participant.invalidData(body)) {
     throw new TypeError('Required attributes are not present');
+  }
+
+  if (participant.invalidYear(body.birthyear)) {
+    throw new TypeError(`Malformed year-of-birth: '${body.birthyear}'. Please provide a 4 digit number i.e. '1969'`);
   }
 
   let p = {
@@ -37,31 +45,31 @@ participant.from = (body) => {
     tshirt: tshirt.from(body),
     goal: body.goal
   };
-  
+
   p.with = function (property) {
     return _.assignIn(p, property);
   };
-  
+
   p.withStartNr = (nr) => {
-   return p.with({start_number: nr});
+    return p.with({start_number: nr});
   };
-  
+
   p.withToken = (token) => {
-   return p.with({paymentToken: token});
+    return p.with({paymentToken: token});
   };
-  
+
   p.withSecureId = (id) => {
-   return p.with({secureID: id});
+    return p.with({secureID: id});
   };
-  
+
   p.withStartBlock = (block) => {
-   return p.with({start_block: block});
+    return p.with({start_block: block});
   };
 
   p.withRegistrationTime = (time) => {
-   return p.with({registrationTime: time});
+    return p.with({registrationTime: time});
   };
-  
+
   return p;
 };
 

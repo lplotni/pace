85bcc58a9fa38fd4fcc7ed6bb040da349177bb83
Christoph <chris@inferno.nadir.org>
2018-04-10 21:15:35 +0200

    startnumbers 1910 and 1717 are special for st. pauli
    These numbers will be given to the St. Pauli football professionals when
    they run for us.

--- service/startNumbers.js
+++ service/startNumbers.js
@@ -18,7 +18,7 @@ numbers.next = () => {
 };
 
 numbers.escape = (nr) => {
-  if (_.includes([18, 28, 74, 84, 88, 444, 191, 192, 198, 420, 1919, 1933, 1488, 1681], nr)) {
+  if (_.includes([18, 28, 74, 84, 88, 444, 191, 192, 198, 420, 1717, 1910, 1919, 1933, 1488, 1681], nr)) {
     return numbers.escape(nr + 1);
   }
   return nr;

7a7f6fe97e8f08b6bdbb06e93bd4bfc958299f68
Philipp Eckel <peckel@thoughtworks.com>
2018-05-05 13:52:23 +0200

    pe update .gitignore & formatting files

--- terraform/ecs/service.tf
+++ terraform/ecs/service.tf
@@ -1,13 +1,13 @@
 resource "aws_ecs_service" "pace-ecs-service" {
-  name            = "${var.ecs-service-name}"
-  iam_role        = "${var.ecs-service-role-arn}"
-  cluster         = "${aws_ecs_cluster.pace-ecs-cluster.id}"
+  name = "${var.ecs-service-name}"
+  iam_role = "${var.ecs-service-role-arn}"
+  cluster = "${aws_ecs_cluster.pace-ecs-cluster.id}"
   task_definition = "${aws_ecs_task_definition.pace-sample-definition.arn}"
-  desired_count   = 1
+  desired_count = 1
 
   load_balancer {
     target_group_arn = "${var.ecs-target-group-arn}"
-    container_port   = 3000
-    container_name   = "pace"
+    container_port = 3000
+    container_name = "pace"
   }
 }

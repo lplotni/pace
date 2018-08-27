835d60d07093eb4228f616f576a98538d7fb48e5
Christoph <chris@inferno.nadir.org>
2018-08-27 19:33:49 +0200

    new secret for the db

--- terraform/kms.tf
+++ terraform/kms.tf
@@ -1,6 +1,7 @@
-data "aws_kms_secret" "db" {
+data "aws_kms_secrets" "db" {
   secret {
     name    = "postgres_password"
-    payload = "AQICAHh367S9ZiGnpGl+Boso0I+MRbhEJP80IaetQLE64Jge0wGtFz6LRfRUYK61nnwCamoIAAAAdTBzBgkqhkiG9w0BBwagZjBkAgEAMF8GCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMSMK6CafM7DPrlhbIAgEQgDKTPjyQRcHtAd6mzUh7R4wbg70NxeNZfC3rHe7tOIDaIF+1idYo3upKDBpVNrSignr1dA=="
+    payload =
+"AQICAHj3pGK3kvjuU2QjfC/NLx1QiGe5X4F2HVmB8WXAXqpj1gFbYumqtgO9l9RzxqWrlf3oAAAAijCBhwYJKoZIhvcNAQcGoHoweAIBADBzBgkqhkiG9w0BBwEwHgYJYIZIAWUDBAEuMBEEDNgxBE6SNuugkU5bfgIBEIBGfb3/486mCrRP+CuF9qFoJ/Pw2Hk/P2WF6EtP/+7xJRlPAUoheUqgsH4VfsaNvKcYeNuwZOStSohKsCw7UVhe5gafXoZcPg=="
   }
 }

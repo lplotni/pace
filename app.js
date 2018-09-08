bfaf007e40a113f70d4c5fd5e1b016457e71fadd
Mario Castellanos <mariocast@posteo.de>
2018-09-08 19:16:14 +0200

    ck/mc WIP:Add team registration. Tests missing

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ let loginRoute = require('./routes/login');
 let logoutRoute = require('./routes/logout');
 
 let registrationRoute = require('./routes/participants/registration');
+let teamsRegistrationRoute = require('./routes/teams/registration');
 let participantsRoute = require('./routes/participants/participants');
 let editParticipantRoute = require('./routes/participants/editParticipant');
 
@@ -118,8 +119,12 @@ passport.use(new LocalStrategy(
     }
 ));
 
+if (config.get('displayTeamRegistration')) {
+  app.use('/registration', teamsRegistrationRoute);
+} else {
+  app.use('/registration', registrationRoute);
+}
 app.use('/', indexRoute);
-app.use('/registration', registrationRoute);
 app.use('/participants', participantsRoute);
 app.use('/results',resultRoute);
 app.use('/certificate',certificateRoute);
---
+++ routes/teams/registration.js
@@ -0,0 +1,45 @@
+/* jshint node: true */
+/* jshint esnext: true */
+/* global Intl*/
+'use strict';
+
+const express = require('express');
+const config = require('config');
+const router = express.Router();
+
+const participant = require('../../domain/participant');
+const calculator = require('../../domain/costCalculator');
+const editUrlHelper = require('../../domain/editUrlHelper');
+const registration = require('../../service/registration');
+
+router.get('/', (req, res) => {
+  registration.isClosed().then(isClosed =>
+    res.render('registration/teamRegistration', {registrationClosed: isClosed, shirts: config.get('shirts.models')}));
+});
+
+router.post('/', (req, res) => {
+  try {
+    const newParticipant = participant.from(req.body);
+    registration.start(newParticipant)
+      .done((result)  =>
+          res.render('registration/success', {
+            name: newParticipant.team,
+            email: newParticipant.email,
+            bank: config.get('contact.bank'),
+            token: result.token,
+            amount: new Intl.NumberFormat('de-DE', {minimumFractionDigits: '2'}).format(calculator.priceFor(newParticipant)),
+            free: false,
+            editUrl: editUrlHelper.generateUrl(result.secureid),
+            startnr: result.startnr
+          }), err => res.render('failure', {
+          message: err.message
+        })
+      );
+  } catch (err) {
+    res.render('failure', {
+      message: err.message
+    });
+  }
+});
+
+module.exports = router;

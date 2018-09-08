383bd8339496f2c86d6607accdc8a887464ee764
Christoph <chris@inferno.nadir.org>
2018-09-09 00:19:19 +0200

    rename module, add rds

---
+++ terraform/persistence/postgres.tf
@@ -0,0 +1,8 @@
+resource "aws_db_instance" "postgres" {
+  allocated_storage    = 10
+  engine               = "postgres"
+  instance_class       = "db.t2.micro"
+  name                 = "db"
+  username             = "root"
+  password             = "i will not write this here ..."
+}

ad45b1a51ca2f9c4b97a50fa3325e003fd6c5fb8
Philipp Eckel <peckel@thoughtworks.com>
2018-09-08 15:37:50 +0200

    PE/CK create upstart job for Cloudwatch agent

--- terraform/iam/ecs-instance-profile.tf
+++ terraform/iam/ecs-instance-profile.tf
@@ -1,7 +1,7 @@
 resource "aws_iam_instance_profile" "ecs-instance-profile" {
-    name = "ecs-instance-profile"
-    path = "/"
-    role = "${aws_iam_role.ecs-instance-role.name}"
+  name = "ecs-instance-profile"
+  path = "/"
+  role = "${aws_iam_role.ecs-instance-role.name}"
 }
 
 output "ecs-instance-profile-name" {
--- terraform/iam/ecs-service-role.tf
+++ terraform/iam/ecs-service-role.tf
@@ -1,23 +1,23 @@
 resource "aws_iam_role" "ecs-service-role" {
-    name                = "ecs-service-role"
-    path                = "/"
-    assume_role_policy  = "${data.aws_iam_policy_document.ecs-service-policy.json}"
+  name               = "ecs-service-role"
+  path               = "/"
+  assume_role_policy = "${data.aws_iam_policy_document.ecs-service-policy.json}"
 }
 
 resource "aws_iam_role_policy_attachment" "ecs-service-role-attachment" {
-    role       = "${aws_iam_role.ecs-service-role.name}"
-    policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
+  role       = "${aws_iam_role.ecs-service-role.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
 }
 
 data "aws_iam_policy_document" "ecs-service-policy" {
-    statement {
-        actions = ["sts:AssumeRole"]
+  statement {
+    actions = ["sts:AssumeRole"]
 
-        principals {
-            type        = "Service"
-            identifiers = ["ecs.amazonaws.com"]
-        }
+    principals {
+      type        = "Service"
+      identifiers = ["ecs.amazonaws.com"]
     }
+  }
 }
 
 output "ecs-service-role-arn" {

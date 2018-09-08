92f0cbf5ae4341ca7ca67e029fe0d3138f8cea54
Christoph <chris@inferno.nadir.org>
2018-09-08 15:22:45 +0200

    allow instances to create cloudwatch log groups (+terraform fmt)

--- terraform/iam/ecs-instance-role.tf
+++ terraform/iam/ecs-instance-role.tf
@@ -15,9 +15,54 @@ data "aws_iam_policy_document" "ecs-instance-policy" {
   }
 }
 
+
+data "aws_iam_policy_document" "ecs-instance-role-attachment" {
+  statement {
+    actions = [
+      "ecs:CreateCluster",
+            "ecs:DeregisterContainerInstance",
+              "ecs:DiscoverPollEndpoint",
+              "ecs:Poll",
+              "ecs:RegisterContainerInstance",
+              "ecs:StartTelemetrySession",
+              "ecs:UpdateContainerInstancesState",
+              "ecs:Submit*",
+              "ecr:GetAuthorizationToken",
+              "ecr:BatchCheckLayerAvailability",
+              "ecr:GetDownloadUrlForLayer",
+              "ecr:BatchGetImage",
+              "logs:CreateLogStream",
+              "logs:PutLogEvents"
+
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "allow_logs" {
+  statement {
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:DescribeLogStreams"
+    ],
+    resources = [
+      "arn:aws:logs:*:*:*"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "allow_logs_policy" {
+  policy = "${data.aws_iam_policy_document.allow_logs.json}"
+  name = "allow_logs_policy"
+}
 resource "aws_iam_role_policy_attachment" "ecs-instance-role-attachment" {
   role       = "${aws_iam_role.ecs-instance-role.name}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs-logs-role-attachment" {
+  role       = "${aws_iam_role.ecs-instance-role.name}"
+  policy_arn = "${aws_iam_policy.allow_logs_policy.arn}"
+
 }
 
 output "ecs-instance-role-name" {

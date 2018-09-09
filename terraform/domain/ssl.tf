4c2dcce9c2396e1d5534f696bf1a1979d5731ede
Christoph <chris@inferno.nadir.org>
2018-09-09 11:43:22 +0200

    we only use the www. domain at the moment

--- terraform/domain/ssl.tf
+++ terraform/domain/ssl.tf
@@ -1,6 +1,5 @@
 resource "aws_acm_certificate" "cert" {
-  domain_name               = "${var.domain_name}"
-  subject_alternative_names = ["${var.domain_name}", "www.${var.domain_name}"]
+  domain_name               = "www.${var.domain_name}"
   validation_method         = "DNS"
 
   tags {
@@ -12,7 +11,7 @@ resource "aws_acm_certificate" "cert" {
   }
 }
 
-resource "aws_route53_record" "cert_validation0" {
+resource "aws_route53_record" "cert_validation" {
   name    = "${aws_acm_certificate.cert.domain_validation_options.0.resource_record_name}"
   type    = "${aws_acm_certificate.cert.domain_validation_options.0.resource_record_type}"
   zone_id = "${data.aws_route53_zone.primary.id}"
@@ -20,17 +19,9 @@ resource "aws_route53_record" "cert_validation0" {
   ttl     = 60
 }
 
-resource "aws_route53_record" "cert_validation1" {
-  name    = "${aws_acm_certificate.cert.domain_validation_options.1.resource_record_name}"
-  type    = "${aws_acm_certificate.cert.domain_validation_options.1.resource_record_type}"
-  zone_id = "${data.aws_route53_zone.primary.id}"
-  records = ["${aws_acm_certificate.cert.domain_validation_options.1.resource_record_value}"]
-  ttl     = 60
-}
-
 resource "aws_acm_certificate_validation" "cert" {
   certificate_arn         = "${aws_acm_certificate.cert.arn}"
-  validation_record_fqdns = ["${aws_route53_record.cert_validation0.fqdn}", "${aws_route53_record.cert_validation1.fqdn}"]
+  validation_record_fqdns = ["${aws_route53_record.cert_validation.fqdn}"]
 }
 
 output "cert-arn" {

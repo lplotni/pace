5266edf94dc6fc4318eba06299f09130c6135c56
Christoph <chris@inferno.nadir.org>
2018-09-09 11:43:56 +0200

    database name is not allowed to be just 'db'

--- terraform/persistence/postgres.tf
+++ terraform/persistence/postgres.tf
@@ -2,7 +2,7 @@ resource "aws_db_instance" "postgres" {
   allocated_storage    = 10
   engine               = "postgres"
   instance_class       = "db.t2.micro"
-  name                 = "db"
+  name                 = "pacedb"
   username             = "root"
   password             = "i will not write this here ..."
 }

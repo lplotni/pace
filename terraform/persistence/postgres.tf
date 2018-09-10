6c209f403a154919a2af22b1a64c97e19e5a8ed0
Philipp Eckel <peckel@thoughtworks.com>
2018-09-10 23:04:41 +0200

    PE/CK setting up Redis and Postgres and appropriate vpc and security groups

--- terraform/persistence/postgres.tf
+++ terraform/persistence/postgres.tf
@@ -1,11 +1,13 @@
 resource "aws_db_instance" "postgres" {
-  allocated_storage = 10
-  engine            = "postgres"
-  engine_version    = "10.4"
-  instance_class    = "db.t2.micro"
-  name              = "pacedb"
-  username          = "root"
-  password          = "${data.aws_secretsmanager_secret_version.superuser_db_password.secret_string}"
+  allocated_storage      = 10
+  engine                 = "postgres"
+  engine_version         = "10.4"
+  instance_class         = "db.t2.micro"
+  name                   = "pacedb"
+  username               = "root"
+  password               = "${data.aws_secretsmanager_secret_version.superuser_db_password.secret_string}"
+  db_subnet_group_name   = "${aws_db_subnet_group.postgres_subnet.id}"
+  vpc_security_group_ids = ["${aws_security_group.postgres-security-group.id}"]
 }
 
 resource "aws_db_parameter_group" "postgres_ssl" {
@@ -15,6 +17,29 @@ resource "aws_db_parameter_group" "postgres_ssl" {
   parameter {
     name  = "rds.force_ssl"
     value = "1"
+  }
+}
+
+resource "aws_security_group" "postgres-security-group" {
+  name = "postgres-security-group"
+
+  description = "RDS postgres servers"
+  vpc_id      = "${var.vpc-id}"
+
+  # Only postgres in
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound traffic.
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
@@ -28,4 +53,13 @@ data "aws_secretsmanager_secret_version" "superuser_db_password" {
 
 output "postgres-ip" {
   value = "${aws_db_instance.postgres.address}"
+}
+
+resource "aws_db_subnet_group" "postgres_subnet" {
+  name       = "pacedb-subnet"
+  subnet_ids = ["${var.db-subnet-id-1}", "${var.db-subnet-id-2}"]
+
+  tags {
+    Name = "db subnet groups"
+  }
 }
--- terraform/persistence/variables.tf
+++ terraform/persistence/variables.tf
@@ -1,3 +1,6 @@
-variable "vpc-subnet-id" {}
-
+variable "redis-subnet-id" {}
+variable "db-subnet-id-1" {}
+variable "db-subnet-id-2" {}
 variable "app_name" {}
+variable "security-group-id" {}
+variable "vpc-id" {}

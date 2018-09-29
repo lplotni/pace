a733d9ad6653f5168408f739ef729a649d2f80d3
Christoph <chris@inferno.nadir.org>
2018-09-29 11:23:14 +0200

    add download link for startnumbers

--- pace-pdf/routes/api.js
+++ pace-pdf/routes/api.js
@@ -8,6 +8,7 @@ const pdfGeneration = require('../pdf/pdfGeneration');
 
 router.get('/startnumbers', (req, res) => {
   res.setHeader('Content-Disposition', 'attachment');
+  res.setHeader('Content-disposition', 'attachment; filename=startnumbers.zip');
   res.setHeader('Content-type', 'application/zip');
   pdfGeneration.zip(res);
 });
